5b83c303cea7699e7f14b2fd0b3d231ba5a9b375
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2019-01-20 21:52:42 +0100

    Add utility code from other files

--- src/aria2p/utils.py
+++ src/aria2p/utils.py
@@ -1,34 +1,58 @@
-STRUCT_KEYS_REVERSE = {
-    "gid": "gid",
-    "status": "status",
-    "totalLength": "total_length",
-    "completedLength": "completed_length",
-    "uploadLength": "upload_length",
-    "bitfield": "bitfield",
-    "downloadSpeed": "download_speed",
-    "uploadSpeed": "upload_speed",
-    "infoHash": "info_hash",
-    "numSeeders": "num_seeders",
-    "seeder": "seeder",
-    "pieceLength": "piece_length",
-    "numPieces": "num_pieces",
-    "connections": "connections",
-    "errorCode": "error_code",
-    "errorMessage": "error_message",
-    "followedBy": "followed_by",
-    "following": "following",
-    "belongsTo": "belongs_to",
-    "dir": "dir",
-    "files": "files",
-    "bittorrent": "bittorrent",
-    "announceList": "announce_list",
-    "comment": "comment",
-    "creationDate": "creation_date",
-    "mode": "mode",
-    "info": "info",
-    "name": "name",
-    "verifiedLength": "verified_length",
-    "verifyIntegrityPending": "verify_integrity_pending",
-}
+"""
+Utils module.
 
-STRUCT_KEYS = {v: k for k, v in STRUCT_KEYS_REVERSE.items()}
+This module contains simple utility functions.
+"""
+
+
+def human_readable_timedelta(value):
+    pieces = []
+
+    if value.days:
+        pieces.append(f"{value.days}d")
+
+    seconds = value.seconds
+
+    if seconds >= 3600:
+        hours = int(seconds / 3600)
+        pieces.append(f"{hours}h")
+        seconds -= hours * 3600
+
+    if seconds >= 60:
+        minutes = int(seconds / 60)
+        pieces.append(f"{minutes}m")
+        seconds -= minutes * 60
+
+    if seconds > 0 or not pieces:
+        pieces.append(f"{seconds}s")
+
+    return "".join(pieces)
+
+
+def human_readable_bytes(value, digits=2, delim="", postfix=""):
+    unit = "B"
+    for u in ("KiB", "MiB", "GiB", "TiB"):
+        if value > 1000:
+            value /= 1024
+            unit = u
+        else:
+            break
+    return f"{value:.{digits}f}" + delim + unit + postfix
+
+
+def bool_or_value(value):
+    if value == "true":
+        return True
+    elif value == "false":
+        return False
+    else:
+        return value
+
+
+def bool_to_str(value):
+    if value is True:
+        return "true"
+    elif value is False:
+        return "false"
+    else:
+        return value
