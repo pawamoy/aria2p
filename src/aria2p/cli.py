ef386a060098e424df2679478aadff099e6356bc
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2019-01-21 22:43:12 +0100

    WIP cli subcommands and args

--- src/aria2p/cli.py
+++ src/aria2p/cli.py
@@ -20,7 +20,107 @@ import json
 import sys
 
 from .api import API
-from .client import JSONRPCClient, JSONRPCError
+from .client import DEFAULT_HOST, DEFAULT_PORT, JSONRPCClient, JSONRPCError
+
+
+# ============ MAIN FUNCTION ============ #
+def main(args=None):
+    """The main function, which is executed when you type ``aria2p`` or ``python -m aria2p``."""
+
+    parser = get_parser()
+    args = parser.parse_args(args=args)
+    kwargs = args.__dict__
+
+    api = API(JSONRPCClient(host=kwargs.pop("host"), port=kwargs.pop("port"), secret=kwargs.pop("secret")))
+
+    subcommands = {
+        None: subcommand_show,
+        "call": subcommand_call,
+        "add-magnet": subcommand_add_magnet,
+        "add-torrent": subcommand_add_torrent,
+        "show": subcommand_show,
+    }
+
+    subcommand = kwargs.pop("subcommand")
+
+    try:
+        return subcommands.get(subcommand)(api, **kwargs)
+    except JSONRPCError as e:
+        print(e.message)
+        return e.code
+
+
+def get_parser():
+    """Return a parser for the command-line options and arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-p", "--port", dest="port", default=DEFAULT_PORT, type=int)
+    parser.add_argument("-H", "--host", dest="host", default=DEFAULT_HOST)
+    parser.add_argument("-s", "--secret", dest="secret", default="")
+
+    subparsers = parser.add_subparsers(dest="subcommand")
+
+    subparsers.add_parser("show", help="Show the download progression.")
+
+    call_parser = subparsers.add_parser("call", help="Call a remote method through the JSON-RPC client.")
+    call_parser.add_argument("method")
+    call_parser_mxg = call_parser.add_mutually_exclusive_group()
+    call_parser_mxg.add_argument("-P", "--params-list", dest="params", nargs="+")
+    call_parser_mxg.add_argument("-J", "--json-params", dest="params")
+
+    add_magnet_parser = subparsers.add_parser("add-magnet", help="Add a download with a Magnet URI.")
+    add_magnet_parser.add_argument("uri")
+
+    add_torrent_parser = subparsers.add_parser("add-torrent", help="Add a download with a Torrent file.")
+    add_torrent_parser.add_argument("torrent_file")
+
+    # sub-commands: list, add, pause, resume, stop, remove, search, info
+
+    return parser
+
+
+# ============ SHOW SUBCOMMAND ============ #
+def subcommand_show(api):
+    downloads = api.get_downloads()
+
+    print(
+        f"{'GID':<17} "
+        f"{'STATUS':<9} "
+        f"{'PROGRESS':>8} "
+        f"{'DOWN_SPEED':>12} "
+        f"{'UP_SPEED':>12} "
+        f"{'ETA':>8}  "
+        f"NAME"
+    )
+
+    for download in downloads:
+        print(
+            f"{download.gid:<17} "
+            f"{download.status:<9} "
+            f"{download.progress_string():>8} "
+            f"{download.download_speed_string():>12} "
+            f"{download.upload_speed_string():>12} "
+            f"{download.eta_string():>8}  "
+            f"{download.name}"
+        )
+
+
+# ============ CALL SUBCOMMAND ============ #
+def subcommand_call(api, method, params):
+    method = get_method(method)
+    if method is None:
+        print(f"Unknown method {method}. Run '{sys.argv[0]} -m listmethods' to list the available methods.")
+        return 1
+
+    if isinstance(params, str):
+        params = json.loads(params)
+    elif params is None:
+        params = []
+
+    response = api.client.call(method, params)
+    print(json.dumps(response))
+
+    return 0
 
 
 def get_method(name, default=None):
@@ -35,92 +135,13 @@ def get_method(name, default=None):
     return methods.get(name, default)
 
 
-def get_parser():
-    """Return a parser for the command-line options and arguments."""
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="subcommand")
-
-    call_parser = subparsers.add_parser("call", help="Call a remote method through the JSON-RPC client.")
-
-    call_parser.add_argument("method")
-
-    call_parser_mxg = call_parser.add_mutually_exclusive_group()
-    call_parser_mxg.add_argument("-p", "--params", dest="params", nargs="+")
-    call_parser_mxg.add_argument("-j", "--json-params", dest="json_params")
-
-    add_magnet_parser = subparsers.add_parser("add-magnet", help="Add a download with a Magnet URI.")
-    add_magnet_parser.add_argument("uri")
-
-    add_torrent_parser = subparsers.add_parser("add-torrent", help="Add a download with a Torrent file.")
-    add_torrent_parser.add_argument("torrent_file")
-
-    # sub-commands: list, add, pause, resume, stop, remove, search, info
-
-    return parser
+# ============ ADD MAGNET SUBCOMMAND ============ #
+def subcommand_add_magnet(api, uri):
+    api.add_magnet(uri)
+    return 0
 
 
-def main(args=None):
-    """The main function, which is executed when you type ``aria2p`` or ``python -m aria2p``."""
-    client = JSONRPCClient()
-    api = API(client)
-
-    parser = get_parser()
-    args = parser.parse_args(args=args)
-
-    try:
-
-        if args.subcommand == "call":
-
-            method = get_method(args.method)
-            if method is None:
-                print(
-                    f"Unknown method {args.method}. Run '{sys.argv[0]} -m listmethods' to list the available methods."
-                )
-                return 1
-
-            params = []
-            if args.params:
-                params = args.params
-            elif args.json_params:
-                params = json.loads(args.json_params)
-
-            response = client.call(method, params)
-            print(json.dumps(response))
-
-            return 0
-
-        elif args.subcommand == "add-magnet":
-            api.add_magnet(args.uri)
-
-        elif args.subcommand == "add-torrent":
-            api.add_torrent(args.torrent_file)
-
-        else:
-            downloads = api.get_downloads()
-
-            print(
-                f"{'GID':<17} "
-                f"{'STATUS':<9} "
-                f"{'PROGRESS':>8} "
-                f"{'DOWN_SPEED':>12} "
-                f"{'UP_SPEED':>12} "
-                f"{'ETA':>8}  "
-                f"NAME"
-            )
-
-            for download in downloads:
-                print(
-                    f"{download.gid:<17} "
-                    f"{download.status:<9} "
-                    f"{download.progress_string():>8} "
-                    f"{download.download_speed_string():>12} "
-                    f"{download.upload_speed_string():>12} "
-                    f"{download.eta_string():>8}  "
-                    f"{download.name}"
-                )
-
-    except JSONRPCError as e:
-        print(e.message)
-        return e.code
-
+# ============ ADD TORRENT SUBCOMMAND ============ #
+def subcommand_add_torrent(api, torrent_file):
+    api.add_torrent(torrent_file)
     return 0
