93068c670fbc60565f3bd41799e0bebe146159f1
Timothée Mazzucotelli <timothee.mazzucotelli@pm.me>
2019-03-09 01:08:35 +0100

    style: Lint and simplify

--- scripts/gen-readme-data.py
+++ scripts/gen-readme-data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import json
 import argparse
+import json
 
 from aria2p.cli import get_parser
 
@@ -9,24 +9,15 @@ parser = get_parser()
 
 aliases = ["autoclear", "clear", "rm"]
 
-output = {
-    "main_usage": parser.format_help(),
-    "commands": []
-}
+output = {"main_usage": parser.format_help(), "commands": []}
 
-subparser_actions = [
-    action for action in parser._actions
-    if isinstance(action, argparse._SubParsersAction)
-]
+subparser_actions = [action for action in parser._actions if isinstance(action, argparse._SubParsersAction)]
 
 for subparser_action in subparser_actions:
     for choice, subparser in subparser_action.choices.items():
         if choice in aliases:
             continue
-        output["commands"].append({
-            "name": choice,
-            "usage": subparser.format_help()
-        })
+        output["commands"].append({"name": choice, "usage": subparser.format_help()})
 
 json_output = json.dumps(output)
 print(json_output)
