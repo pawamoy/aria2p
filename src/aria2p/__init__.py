98acbe7792938c933e6df8a583f6e09cfcb2f7de
Timothée Mazzucotelli <pawamoy@pm.me>
2019-09-06 18:53:44 +0200

    chore: Move TODOs to GitHub issues

--- src/aria2p/__init__.py
+++ src/aria2p/__init__.py
@@ -19,22 +19,3 @@ from .stats import Stats
 logger.disable("aria2p")
 
 __all__ = ["API", "ClientException", "Client", "Download", "BitTorrent", "File", "Options", "Stats"]
-
-# TODO: use proper logging messages (esp. in except: pass)
-# TODO: maybe add type annotations
-# TODO: handle both str and pathlib.Path for paths consistently
-# TODO: add command "add" for normal downloads!!
-# TODO: add "--options" options for "add" commands
-# TODO: in API, support download arguments to be both Download or str (GID)?
-# TODO: add clean parameter for api.move_files method (to clean .aria2 files)
-# TODO: add value verification for options (see man page)
-# TODO: check_args must check that commands like rm have arguments
-
-# Roadmap:
-# - feature: Ability to hide metadata downloads (magnet URIs)
-# - feature: Ability to move files to another directory
-# - feature: Ability to tag downloads with markers (like categories)
-# - feature: Ability to execute instructions on events (download completed, etc.)
-# - feature: Improved queue
-# - feature: Interactive display (htop-style) with sorting, filtering and actions
-# - feature: Ability to search current downloads with patterns and filters on fields
