18ca44c0bfbe03c3d6cadf47f33dce1295074672
Timothée Mazzucotelli <pawamoy@pm.me>
2019-10-17 16:01:11 +0200

    tests: Write logs during tests, set as artifacts on failure on GitLab CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+
+import pytest
+
+from aria2p import enable_logger
+
+
 def pytest_itemcollected(item):
     item._nodeid = (
         item._nodeid.replace(".py", "")
@@ -11,3 +18,27 @@ def pytest_itemcollected(item):
     doc = item.obj.__doc__.strip() if item.obj.__doc__ else ""
     if doc:
         item._nodeid = item._nodeid.split("::")[0] + "::" + doc
+
+
+@pytest.fixture(autouse=True)
+def gitlab_logs(request):
+    # put logs in tests/logs
+    log_path = Path("tests") / "logs"
+
+    # tidy logs in subdirectories based on test module and class names
+    module = request.module
+    class_ = request.cls
+    name = request.node.name + ".log"
+
+    if module:
+        log_path /= module.__name__.replace("tests.", "")
+    if class_:
+        log_path /= class_.__name__
+
+    log_path.mkdir(parents=True, exist_ok=True)
+
+    # append last part of the name and enable logger
+    log_path /= name
+    if log_path.exists():
+        log_path.unlink()
+    enable_logger(sink=log_path, level="TRACE")
