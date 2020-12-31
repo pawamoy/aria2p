61a53bccb180507f45ffc12511d8bb8047d118ed
blackCauldron7 <blackCauldron7@protonmail.com>
2020-12-31 22:40:07 +0530

    Added new color value DEFAULT = -1

--- src/aria2p/interface.py
+++ src/aria2p/interface.py
@@ -75,6 +75,9 @@ def color_palette_parser(palette: str) -> Tuple[int, int, int]:
         "YELLOW": Screen.COLOUR_YELLOW,
         "BLUE": Screen.COLOUR_BLUE,
         "GREEN": Screen.COLOUR_GREEN,
+        "DEFAULT": -1, # -1 stands for the standard terminfo color which means use
+        # the default color provided by the terminal this allows for terminals
+        # with transparency to allow tranparent background color
     }
     mode_map = {
         "NORMAL": Screen.A_NORMAL,
