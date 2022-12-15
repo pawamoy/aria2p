068774ff32b3820133f8926d204cc7b9a65f169a
Timothée Mazzucotelli <pawamoy@pm.me>
2022-12-15 21:34:35 +0100

    Deployed 5c91827 with MkDocs version: 1.4.2

--- gen_ref_nav.py
+++ gen_ref_nav.py
@@ -6,23 +6,30 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("src").glob("**/*.py")):
+for path in sorted(Path("src").rglob("*.py")):
     module_path = path.relative_to("src").with_suffix("")
     doc_path = path.relative_to("src", "aria2p").with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
-    parts = list(module_path.parts)
-    parts[-1] = f"{parts[-1]}.py"
-    nav[parts] = doc_path
+    parts = tuple(module_path.parts)
+
+    if parts[-1] == "__init__":
+        parts = parts[:-1]
+        doc_path = doc_path.with_name("index.md")
+        full_doc_path = full_doc_path.with_name("index.md")
+    elif parts[-1] == "__main__":
+        continue
+
+    nav[parts] = doc_path.as_posix()
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        ident = ".".join(module_path.parts)
-        print("::: " + ident, file=fd)
+        ident = ".".join(parts)
+        fd.write(f"::: {ident}")
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
 # add pages manually:
 # nav["package", "module"] = "path/to/file.md"
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open("reference/SUMMARY.txt", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
